233f4596a14e6106aa65544ee80e2dfbbba5e524
pmazurki <pmazurki@me.com>
2020-01-12 15:30:30 +0000

    Update test.ps1

--- install/test.ps1
+++ install/test.ps1
@@ -1,4 +1,9 @@
+function Test{
+    [Alias('Test')]
+    Param(<#NoParams#>)
+    # Sub CustomObj
 Write-Host " "
 Write-Host "Here are the top 10 CPU consuming processes right now"
 Write-Host " "
 Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
+}
