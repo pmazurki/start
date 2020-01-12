dc09a48a8f85f5b553ecd504142c654749078d8a
pmazurki <pmazurki@me.com>
2020-01-12 16:08:49 +0000

    Update test.ps1

--- install/test.ps1
+++ install/test.ps1
@@ -1,9 +1,9 @@
-function Test-fn{
-    [Alias('Test-fn')]
+ function Fn-Test{
+    
     Param(<#NoParams#>)
-    # Sub CustomObj
+
 Write-Host " "
 Write-Host "Here are the top 10 CPU consuming processes right now"
 Write-Host " "
 Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
-} 
+}  
