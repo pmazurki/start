ac7e5ab6fb3aef35ab0b2657f3bdb613b6d8252a
pmazurki <pmazurki@me.com>
2020-01-12 14:49:47 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 echo Start
 echo install jumpdesktop
-powershell.exe "iex (iwr https://xxx.ps1);start" >> C:\Windows\Temp\Start.txt
+powershell.exe "iex (https://raw.githubusercontent.com/pmazurki/start/master/install/app.ps1);start" >> C:\Windows\Temp\Start.txt
 echo install COMPLETED
